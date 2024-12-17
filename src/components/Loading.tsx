80a9072c4ecbd63c21f3bf0e1c77e8b1fe01ccde
mafortthiago <thmafort@gmail.com>
2024-12-17 13:00:40 -0300

    style(Loading): Add margin above

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import "../styles/Loading.css";
 import { t } from "i18next";
 const Loading: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center mt-4">
       <div className="matrix">
         {Array.from({ length: 9 }).map((_, index) => (
           <div key={index} className="cell" id={`cell${index}`}></div>
