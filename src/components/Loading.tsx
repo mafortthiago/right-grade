4ec20bfdba5bf5c955e8c0705917808bdadd7d07
mafortthiago <thmafort@gmail.com>
2024-12-17 12:51:15 -0300

    feat(components): add loading component:

---
+++ src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Loading.css";
+import { t } from "i18next";
+const Loading: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="matrix">
+        {Array.from({ length: 9 }).map((_, index) => (
+          <div key={index} className="cell" id={`cell${index}`}></div>
+        ))}
+      </div>
+      <p>{t("loading")}</p>
+    </div>
+  );
+};
+
+export default Loading;
