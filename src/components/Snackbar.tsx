516e7dd74e52947112f4a8c1922c2b093c14b42c
mafortthiago <thmafort@gmail.com>
2024-12-19 14:08:59 -0300

    feat(components): Create and style snackbar component

---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,60 @@
+import { FunctionComponent, useContext, useEffect, useState } from "react";
+import { BsCheckSquareFill, BsXSquareFill } from "react-icons/bs";
+import { themeContext } from "../context/ThemeContext";
+interface SnackbarProps {
+  title: string;
+  body: string;
+  isError: boolean;
+  setSnackbarVisible: (isVisible: boolean) => void;
+}
+const Snackbar: FunctionComponent<SnackbarProps> = ({
+  isError,
+  title,
+  body,
+  setSnackbarVisible,
+}) => {
+  const [seconds, setSeconds] = useState<number>(10);
+  const { theme } = useContext(themeContext);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds: number) => {
+        if (prevSeconds === 1) {
+          clearInterval(interval);
+          setSnackbarVisible(false);
+          return 0;
+        }
+        return prevSeconds - 1;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div
+      className={`animate-slide-in-left flex items-center top-20 absolute max-sm:w-5/6 clip-path-snackbar md:top-24 md:left-8 z-20 bg-gradient-to-r p-2 rounded-lg ${
+        theme == "dark"
+          ? "to-gray-900 from-zinc-900"
+          : "to-zinc-100 from-slate-100"
+      }`}
+    >
+      {isError ? (
+        <BsXSquareFill className={`w-10 h-10 text-red-400 mr-2`} />
+      ) : (
+        <BsCheckSquareFill className={`w-10 h-10 text-green-700 mr-2`} />
+      )}
+
+      <section>
+        <h3>{title}</h3>
+        <p className="text-sm">{body}</p>
+      </section>
+      <span className="text-xs flex items-center ml-2 md:ml-4 md:text-sm">
+        {seconds}s
+      </span>
+      <div
+        className={`w-2 bg-first ml-1 rounded`}
+        style={{ height: `${seconds * 4}px` }}
+      ></div>
+    </div>
+  );
+};
+
+export default Snackbar;
