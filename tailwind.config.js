9f764b86f1441117840a6511626e178448714fd6
mafortthiago <thmafort@gmail.com>
2024-12-19 14:05:19 -0300

    feat(style): Add slide-in-left animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      keyframes: {
+        "slide-in-left": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "slide-in-left": "slide-in-left 0.5s ease-out forwards",
+      },
       fontFamily: {
         rubik: ["Rubik", "sans-serif"],
       },
