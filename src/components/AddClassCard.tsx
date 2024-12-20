bc7caa9e9b28b3586cd5997659b25ce9a65a7714
mafortthiago <thmafort@gmail.com>
2024-12-20 14:34:42 -0300

    feat(components): Implement class addition card

---
+++ src/components/AddClassCard.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent, useContext } from "react";
+import { themeContext } from "../context/ThemeContext";
+import { BsPlusSquare } from "react-icons/bs";
+interface AddClassCardProps {
+  isAddClass: boolean;
+  setIsAddClass: (isAddClass: boolean) => void;
+}
+const AddClassCard: FunctionComponent<AddClassCardProps> = ({
+  isAddClass,
+  setIsAddClass,
+}) => {
+  const { theme } = useContext(themeContext);
+  return (
+    <div
+      onClick={() => setIsAddClass(!isAddClass)}
+      className={
+        "flex flex-col cursor-pointer mt-6 p-4 rounded m-4 border-2 outline-4 outline w-60 h-32 hover:shadow-2xl items-center justify-center " +
+        (theme === "dark"
+          ? "bg-third outline-third border-light-100 hover:shadow-gray-800"
+          : "bg-light-100 outline-light-100 hover:shadow-gray-400")
+      }
+    >
+      <h3>Cadastrar nova turma</h3>
+      <BsPlusSquare className="w-12 h-10 mt-2 text-gray-300" />
+    </div>
+  );
+};
+
+export default AddClassCard;
