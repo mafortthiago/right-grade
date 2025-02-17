259677b2cd9645f15b8ce7bbf830da910842fa32
mafortthiago <thmafort@gmail.com>
2025-02-17 20:09:27 -0300

    feat(CardClass): enable navigation to group page when clicking

--- src/components/CardClass.tsx
+++ src/components/CardClass.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent, useContext } from "react";
 import { themeContext } from "../context/ThemeContext";
+import { useNavigate } from "react-router-dom";
 
 interface CardProps {
+  id: string;
   title: string;
   quantityStudents: number;
   gradeAverage: number;
@@ -11,8 +13,13 @@ const CardClass: FunctionComponent<CardProps> = ({
   title,
   quantityStudents,
   gradeAverage,
+  id,
 }) => {
   const { theme } = useContext(themeContext);
+  const navigate = useNavigate();
+  const handleCardDetail = () => {
+    navigate(`/group/${id}`);
+  };
   return (
     <>
       <div
@@ -22,6 +29,7 @@ const CardClass: FunctionComponent<CardProps> = ({
             ? "bg-third outline-third border-light-100 hover:shadow-gray-800"
             : "bg-light-100 outline-light-100 hover:shadow-gray-400")
         }
+        onClick={handleCardDetail}
       >
         <h3 className="font-medium border-b-2 border-first mb-2">{title}</h3>
         <p className="text-sm">Alunos: {quantityStudents}</p>
