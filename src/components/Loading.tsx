f6c2a012f1227cf539725e5714c2e939d34370e7
mafortthiago <thmafort@gmail.com>
2025-02-22 18:01:12 -0300

    style(Loading): add size configuration (small and large)

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "../styles/Loading.css";
 import { t } from "i18next";
-const Loading: React.FC = () => {
+
+interface LoadingProps {
+  size: "small" | "large";
+}
+
+const Loading: React.FC<LoadingProps> = ({ size }) => {
   return (
     <div className="flex flex-col items-center justify-center mt-4">
-      <div className="matrix">
+      <div className={`matrix_${size}`}>
         {Array.from({ length: 9 }).map((_, index) => (
-          <div key={index} className="cell" id={`cell${index}`}></div>
+          <div key={index} className={`cell_${size}`} id={`cell${index}`}></div>
         ))}
       </div>
-      <p>{t("loading")}</p>
+      <p className={`${size === "small" && "text-xs"}`}>{t("loading")}</p>
     </div>
   );
 };
