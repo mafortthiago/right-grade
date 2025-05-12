c25e1a046e9cab3cbc41b1f7cc0296fa201ac329
mafortthiago <thmafort@gmail.com>
2025-05-12 11:15:38 -0300

    feat(auth): implement confirm account functionality

--- src/store/authentication/functions/logout.ts
+++ src/store/authentication/functions/logout.ts
@@ -1,7 +1,16 @@
 import { useAuthStore } from "../auth";
 import { useClassStore } from "../../classes";
+import { URL_API } from "../../URL_API";
 
-export const logout = () => {
+export const logout = async () => {
   useAuthStore.setState({ token: "", isAuthenticated: false, id: "" });
   useClassStore.setState({ groups: [] });
+
+  await fetch(URL_API + "/auth/logout", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
--- src/store/authentication/functions/register.ts
+++ src/store/authentication/functions/register.ts
@@ -1,8 +1,10 @@
+import { t } from "i18next";
 import { ApiError } from "../../../errors";
+import { triggerGlobalSnackbar } from "../../../util/globalSnackbar";
 import { generateFetch } from "../../students/functions/updateAPI";
 import { URL_API } from "../../URL_API";
-import { useAuthStore } from "../auth";
 import { UserRegister } from "../interfaces/UserRegister";
+import i18n from "../../../lib/i18n";
 
 export const register = async (user: UserRegister) => {
   const response = await generateFetch(`${URL_API}/register`, "POST", user);
@@ -12,10 +14,27 @@ export const register = async (user: UserRegister) => {
     throw new ApiError(JSON.stringify(responseError));
   }
 
-  const data = await response.json();
-
-  useAuthStore.setState({
-    isAuthenticated: true,
-    id: data.id,
+  await sendConfirmEmail(user.email);
+  triggerGlobalSnackbar({
+    title: t("success"),
+    body: t("authentication.register.confirmEmail", { email: user.email }),
+    isError: false,
   });
+};
+
+const sendConfirmEmail = async (email: string) => {
+  const response = await fetch(`${URL_API}/auth/confirm-account`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept-Language": i18n.language,
+    },
+    body: JSON.stringify({ email }),
+  });
+
+  if (!response.ok) {
+    const r = await response.json();
+
+    throw new ApiError(JSON.stringify(r));
+  }
 };
