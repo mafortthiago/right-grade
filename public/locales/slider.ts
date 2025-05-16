6b761bba17535661a6a5a6329a701700f3966eb9
mafortthiago <thmafort@gmail.com>
2025-05-16 10:43:51 -0300

    feat: add slider images

---
+++ public/locales/slider.ts
@@ -0,0 +1,16 @@
+export default {
+  images: [
+    {
+      src: "https://images.unsplash.com/photo-1606327054536-e37e655d4f4a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      altKey: "home.alt.image1",
+    },
+    {
+      src: "https://lh3.googleusercontent.com/pw/AP1GczNxYFIB7KhMuDwuMkQyBLzBi2mOdhWxKPNj4rlX0IKdFcvDY65NLMSaMAecLGZNov6xRr9UrHrgQDPOIONkC1u9TPBozO5Osu5CPC_8gBJ-LX63pFhegusL0oiUX-xQCtDeTWks4DujtTbkNzJ6H94B=w1065-h599-s-no-gm?authuser=0",
+      altKey: "home.alt.image2",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1586892477838-2b96e85e0f96?q=80&w=1459&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      altKey: "home.alt.image3",
+    },
+  ],
+};
